f73a80fcf9743967973780a3aa6e7db06d668e36
cacutler <116305038+cacutler@users.noreply.github.com>
2025-04-04 14:18:29 -0600

    Trying to figure out the desktop footer styling

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Portfolio</title>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+        <link href="style.css" rel="stylesheet" type="text/css" media="all">
+        <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+    </head>
+    <body>    
+        <div id="wrapper">
+            <header>
+                <nav>
+                    <div>
+                        <ul>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">About Me</a></li>
+                            <li><a href="#"><img id="logo" src="images/logo.svg" alt="logo"></a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                <img src="images/randomperson.jpg" alt="selfie" id="hero">
+            </header>
